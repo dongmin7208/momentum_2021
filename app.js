9c24405e5a5275eec21269faf78d98280d591b14
dongmin7208 <sayg3o3d@gmail.com>
2021-07-03 12:39:51 +0900

    Add color change

--- app.js
+++ app.js
@@ -1,14 +1,14 @@
 const h1 = document.querySelector("div.hello:first-child h1");
 // h1.style.color = "blue";
 function handleTitleClick() {
-    console.log(h1.style.color);
-    h1.innerText = "blue";
-    console.log(h1.style.color);
-    // console.log("title was clicked!");
-}
-function handleMouseEnter() {
-    h1.innerText = "Mouse is here!";
-    // console.log("mouse is here!");
+    const currentColor = h1.style.color;
+    let newColor;
+    if (currentColor === "blue") {
+        newColor = "red";
+    } else {
+        newColor = "blue";
+    }
+    h1.style.color = newColor;
 }
 
 h1.addEventListener("click", handleTitleClick);
