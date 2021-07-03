ae20fd31be015798296c57d862b6c87d3fc79210
dongmin7208 <sayg3o3d@gmail.com>
2021-07-03 13:19:14 +0900

    Add toggle

--- app.js
+++ app.js
@@ -1,14 +1,6 @@
 const h1 = document.querySelector("div.hello:first-child h1");
-// h1.style.color = "blue";
 function handleTitleClick() {
-    const currentColor = h1.style.color;
-    let newColor;
-    if (currentColor === "blue") {
-        newColor = "red";
-    } else {
-        newColor = "blue";
-    }
-    h1.style.color = newColor;
+    h1.classList.toggle("clicked");
 }
 
 h1.addEventListener("click", handleTitleClick);
