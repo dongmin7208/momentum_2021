469e12ec83347b4224b724a6b5a9dd260505a32f
dongmin7208 <sayg3o3d@gmail.com>
2021-07-05 09:49:57 +0900

    Add querySelector.input and button

--- app.js
+++ app.js
@@ -1,4 +1,12 @@
-//const loginForm = document.querySelector("#login-form");같은데 # 넣어줘야함.
-const loginForm = document.getElementById("login-form");
-const loginInput = loginForm.querySelector("input");
-const loginButton = loginForm.querySelector("button");
+// const loginForm = document.querySelector("#login-form");같은데 # 넣어줘야함.
+// const loginForm = document.getElementById("login-form");
+// const loginInput = loginForm.querySelector("input");
+// const loginButton = loginForm.querySelector("button");
+
+const loginInput = document.querySelector("#login-form input");
+const loginButton = document.querySelector("#login-form button");
+
+function onLoginBtnClick() {
+    console.log("click!!!!");
+}
+loginButton.addEventListener("click", onLoginBtnClick);
