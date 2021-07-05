983789f5ab1c8876277318b909fb17fcc69f80f4
dongmin7208 <sayg3o3d@gmail.com>
2021-07-05 10:18:27 +0900

    Add querySelector console

--- app.js
+++ app.js
@@ -7,6 +7,7 @@ const loginInput = document.querySelector("#login-form input");
 const loginButton = document.querySelector("#login-form button");
 
 function onLoginBtnClick() {
+    console.dir(loginInput);
     console.log("click!!!!");
 }
 loginButton.addEventListener("click", onLoginBtnClick);
