e31f59b34bd3ac2b210f74ffbe798aec78425174
dongmin7208 <sayg3o3d@gmail.com>
2021-07-07 11:47:44 +0900

    add class=hidden

--- app.js
+++ app.js
@@ -6,8 +6,11 @@
 const loginForm = document.querySelector("#login-form");
 const loginInput = document.querySelector("#login-form input");
 
+const link = document.querySelector("a");
+
 function onLoginBtnClick(event) {
     event.preventDefault();
 console.log(loginInput.value);
 }
+
 loginForm.addEventListener("submit", onLoginSubmit);
