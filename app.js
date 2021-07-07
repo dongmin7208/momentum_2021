5f20d2d2e6a1d39436fd7ebb0c62e3554f3431ab
dongmin7208 <sayg3o3d@gmail.com>
2021-07-07 14:24:34 +0900

    add hidden=class ,

--- app.js
+++ app.js
@@ -9,10 +9,11 @@ const greeting = document.querySelector("#greeting");
 const HIDDEN_CLASSNAME = "hidden";
 function onLoginSubmit(event) {
     event.preventDefault();
-    loginForm.classList.add("hidden");
+    loginForm.classList.add(HIDDEN_CLASSNAME);
     const username = loginInput.value;
-    greeting.innerText = "Hello  " + username;
-    //greeting.classList.remove("hidden");
+    //greeting.innerText = "Hello  " + username;
+    greeting.innerText = `Hello   + ${username}`;
+    greeting.classList.remove(HIDDEN_CLASSNAME);
 }
 
 loginForm.addEventListener("submit", onLoginSubmit);
