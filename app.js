c1856619a496d0947dc8be6598173ed3737925c1
dongmin7208 <sayg3o3d@gmail.com>
2021-07-07 16:03:00 +0900

    add localStorage

--- app.js
+++ app.js
@@ -7,13 +7,27 @@ const loginForm = document.querySelector("#login-form");
 const loginInput = document.querySelector("#login-form input");
 const greeting = document.querySelector("#greeting");
 const HIDDEN_CLASSNAME = "hidden";
+const USERNAME_KEY = "username";
+
+
 function onLoginSubmit(event) {
     event.preventDefault();
     loginForm.classList.add(HIDDEN_CLASSNAME);
     const username = loginInput.value;
-    //greeting.innerText = "Hello  " + username;
+    // === greeting.innerText = "Hello  " + USERNAME_KEY;
+    localStorage.setItem("HIDDEN_CLASSNAME", username);
     greeting.innerText = `Hello   + ${username}`;
     greeting.classList.remove(HIDDEN_CLASSNAME);
 }
 
-loginForm.addEventListener("submit", onLoginSubmit);
+
+const saveUsername = localStorage.getItem("USERNAME_KEY");
+
+if(saveUsername === null){
+    loginForm.classList.remove(HIDDEN_CLASSNAME);   
+    loginForm.addEventListener("submit", onLoginSubmit);
+ }else{
+     greeting.innerText = `Hello ${saveUsername}`;
+     greeting.classList.remove(HIDDEN_CLASSNAME);
+
+ }
