61d6db7ce056f40b604bb6945a6c63b40ed71c61
dongmin7208 <sayg3o3d@gmail.com>
2021-07-08 20:44:35 +0900

    Add todo-delete

--- js/todo.js
+++ js/todo.js
@@ -3,12 +3,26 @@ const toDoInput = document.querySelector("#todo-form input");
 // const toDoInput = document.querySelector("#todo-form");
 const toDoList = document.getElementById("todo-list");
 
+function deleteToDo(event){
+const li = event.target.parentElement;
+li.remove();
+// console.log(event)
+// console.log(event.target)
+// console.log(event.target.parentElement)
+// console.dir(event.target.parentElement.innerText)
+}
+
 function paintToDo(newTodo){
     const li = document.createElement("li");
     const span = document.createElement("span");
-    li.appendChild(span);
     span.innerText = newTodo;
+    const button = document.createElement("button");
+    button.innerText= "❌"
+    button.addEventListener("click",deleteToDo )
+    li.appendChild(span);
+    li.appendChild(button);
     toDoList.appendChild(li);
+    //순서
 }
 
 function handleToDoSubmit(event){
