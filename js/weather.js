7b03307ccba05ff1b3a469ea8468667c2d0fa6f6
dongmin7208 <sayg3o3d@gmail.com>
2021-07-09 13:34:26 +0900

    Fix  weather

--- js/weather.js
+++ js/weather.js
@@ -1,11 +1,26 @@
+const API_KEY= "889601d91980a0dead4237d7f58399a1";
+
 function onGeoOk(position) {
     const lat = position.coords.latitude;
-    const lng = position.coords.longitude;
-    console.log("住んでいる所", lat, lng);
+    const lon = position.coords.longitude;
     
+    const url = `http://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${API_KEY}&units=metric`;
+    //lat,lon,api
+    fetch(url)
+    .then((response) => response.json())
+    .then((data) => {
+        const weather = document.querySelector("#weather span:first-child");
+        const city = document.querySelector("#weather span:last-child");
+        city.innerText =data.name;
+        weather.innerText = `天気は"${data.weather[0].main}"__気温は"${data.main.temp}"度__湿度は"${data.main.humidity}"`;
+    });
 }
 function onGeoError() {
     alert("位置情報サービス/オフ")
 }
 
 navigator.geolocation.getCurrentPosition(onGeoOk, onGeoError);
+
+
+
+  
